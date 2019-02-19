c2033e128ea4514afa3de5807d5aa98249f5cf53
Viliam Letavay <vilco.letavay@gmail.com>
2019-02-19 13:45:01 +0100

    Installation commands extracted to function. Added appendLine function

--- install.sh
+++ install.sh
@@ -1,45 +1,59 @@
 #!/bin/bash
 
-set -x
-set -e
+function appendLine {
+    FILENAME="$1"
+    LINE="$2"
+    if [ -e "$FILENAME" ]
+    then
+        grep -q "^\s*$LINE" "$FILENAME" || echo "$LINE" >> "$FILENAME"
+    else
+        echo "$LINE" >> "$FILENAME"
+    fi
+}
 
-cd /tmp
+function run_install {
+    set -x
+    set -e
 
-apt update
-apt upgrade -y
+    apt update
+    apt upgrade -y
 
-# Install OpenJDK 11
-add-apt-repository ppa:openjdk-r/ppa
-apt update -q
-apt install -y openjdk-11-jdk
+    # Install OpenJDK 11
+    add-apt-repository -y ppa:openjdk-r/ppa
+    apt update -q
+    apt install -y openjdk-11-jdk
 
-# Install Dotnet SDK 2.2
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-add-apt-repository universe
-apt install -y apt-transport-https 
-apt update
-apt install -y dotnet-sdk-2.2
-echo 'DOTNET_CLI_TELEMETRY_OPTOUT=1' >> /etc/environment
+    # Install Dotnet SDK 2.2
+    wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+    dpkg -i packages-microsoft-prod.deb
+    add-apt-repository -y universe
+    apt install -y apt-transport-https 
+    apt update
+    apt install -y dotnet-sdk-2.2
+    appendLine 'DOTNET_CLI_TELEMETRY_OPTOUT=1' /etc/environment
 
-# Install GCC/G++
-apt install -y gcc g++ build-essential
+    # Install GCC/G++
+    apt install -y gcc g++ build-essential
 
-# Install Python
-apt install -y python3.6 python3-pip
+    # Install Python
+    apt install -y python3.6 python3-pip
 
-# Check installed core packages
-java --version
-dotnet --version
-gcc --version
-g++ --version
-python3 --version
-pip3 --version
+    # Check installed core packages
+    java --version
+    dotnet --version
+    gcc --version
+    g++ --version
+    python3 --version
+    pip3 --version
 
-# Install miscellaneous packages
-apt install -y htop git
+    # Install miscellaneous packages
+    apt install -y htop git
 
-# Remove tracks
-rm /root/.bash_history || true
-rm /home/student/.bash_history || true
-rm -rf /home/student/.ssh || true
+    # Remove tracks
+    rm /root/.bash_history || true
+    rm /home/student/.bash_history || true
+    rm -rf /home/student/.ssh || true
+}
+
+
+(cd /tmp; run_install)
