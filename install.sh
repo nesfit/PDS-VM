9a0db581b26bdf99df72a9e14800e937e8e28341
Viliam Letavay <vilco.letavay@gmail.com>
2019-02-19 13:01:49 +0100

    Init commit

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -x
+set -e
+
+apt update
+apt upgrade -y
+
+# Install OpenJDK 11
+add-apt-repository ppa:openjdk-r/ppa
+apt update -q
+apt install -y openjdk-11-jdk
+
+# Install Dotnet SDK 2.2
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+add-apt-repository universe
+apt install -y apt-transport-https 
+apt update
+apt install -y dotnet-sdk-2.2
+echo 'DOTNET_CLI_TELEMETRY_OPTOUT=1' >> /etc/environment
+
+# Install GCC/G++
+apt install -y gcc g++ build-essential
+
+# Install Python
+apt install -y python3.6 python3-pip
+
+# Check installed core packages
+java --version
+dotnet --version
+gcc --version
+g++ --version
+python3 --version
+pip3 --version
+
+# Install miscellaneous packages
+apt install -y htop git
+
+# Remove tracks
+rm /root/.bash_history || true
+rm /home/student/.bash_history || true
+rm -rf /home/student/.ssh || true
---
+++ readme.txt
@@ -0,0 +1,15 @@
+VirtualBox: 5.2.26
+Ubuntu: ubuntu-18.04.1-desktop-amd64; minimal install; student/student
+
+Software:
+	OpenJDK11
+	Dotnet SDK 2.2
+	gcc 7.3.0, g++ 7.3.0
+	Python 3.6.7, Pip3 
+	git 2.17.1
+	VS Code
+
+
+Possible problems:
+	VT-x is not available (VERR_VMX_NO_VMX):
+		https://www.howtogeek.com/213795/how-to-enable-intel-vt-x-in-your-computers-bios-or-uefi-firmware/
